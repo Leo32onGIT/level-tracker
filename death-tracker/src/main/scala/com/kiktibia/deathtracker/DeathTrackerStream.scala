e6443dfb9b55bce3170f604e321db8170c8b2cd6
Leo32 <bwils120@hotmail.com>
2022-10-02 15:20:31 +1000

    finished adding exiva tags to bottom of pvp deaths

--- death-tracker/src/main/scala/com/kiktibia/deathtracker/DeathTrackerStream.scala
+++ death-tracker/src/main/scala/com/kiktibia/deathtracker/DeathTrackerStream.scala
@@ -181,9 +181,9 @@ class DeathTrackerStream(deathsChannel: TextChannel)(implicit ex: ExecutionConte
       if (exivaBuffer.nonEmpty) {
         exivaBuffer.zipWithIndex.foreach { case (exiva, i) =>
           if (i == 0){
-            exivaList += s"""\n<:exiva:1025866744918716416> `exiva "$exiva"`"""
+            exivaList += s"""\n<:exiva:1025866744918716416>`exiva "$exiva"`""" // add exiva emoji
           } else {
-            exivaList += s"""\n<:indent:1025915320285798451> `exiva "$exiva"`"""
+            exivaList += s"""\n<:indent:1025915320285798451>`exiva "$exiva"`""" // just use indent emoji for further player names
           }
         }
       }
