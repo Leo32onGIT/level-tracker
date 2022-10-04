8013907ecc4df75935e6d7a792a91d4a28a1ad46
Leo32 <bwils120@hotmail.com>
2022-10-05 08:19:02 +1000

    Update DeathTrackerStream.scala

--- death-tracker/src/main/scala/com/kiktibia/deathtracker/DeathTrackerStream.scala
+++ death-tracker/src/main/scala/com/kiktibia/deathtracker/DeathTrackerStream.scala
@@ -259,7 +259,7 @@ class DeathTrackerStream(deathsChannel: TextChannel)(implicit ex: ExecutionConte
       embed.setTitle(s"$charName ${vocEmoji(charDeath.char)}", charUrl(charName))
       embed.setDescription(embedText)
       embed.setThumbnail(embedThumbnail)
-      embed.setFooter(s"<R:$epochSecond>")
+      embed.setFooter(s"<t:$epochSecond:R>")
       embed.setColor(embedColor)
       embed.build()
     }
