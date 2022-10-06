3c0ec38206339db20c13b379c1419e796ae7ff1d
Leo32 <bwils120@hotmail.com>
2022-10-07 00:53:27 +1000

    Update LevelTrackerStream.scala

--- level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
+++ level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
@@ -48,9 +48,10 @@ class LevelTrackerStream(levelsChannel: TextChannel)(implicit ex: ExecutionConte
 
   private lazy val getCharacterData = Flow[WorldResponse].mapAsync(1) { worldResponse =>
     val now = ZonedDateTime.now()
-    val online: List[(String, Double)] = worldResponse.worlds.world.online_players.map(i => (i.name, i.level))
+    val online: List[CharKey] = worldResponse.worlds.world.online_players.map(i => CharKey(i.name, Levels(now.toString, i.level)))
+		logger.info(online)
     recentOnline.filterInPlace(i => !online.contains(i.char)) // Remove existing online chars from the list...
-    recentOnline.addAll(online.map(i => CharKey(i(0), Levels(now.toString, i(1))))) // ...and add them again, with an updated online time
+    recentOnline.addAll(online.map(i => CharKey(i.level.name, Levels(now.toString, i.level.level)))) // ...and add them again, with an updated online time
     val charsToCheck: Set[String] = recentOnline.map(_.char).toSet
     Source(charsToCheck).mapAsyncUnordered(24)(tibiaDataClient.getCharacter).runWith(Sink.collection).map(_.toSet)
   }.withAttributes(logAndResume)
