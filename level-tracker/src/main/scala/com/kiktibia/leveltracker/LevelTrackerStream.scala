3ee4ed7cf43ffa6ceb099024d80ff4f7ffc78c47
Leo32 <bwils120@hotmail.com>
2022-10-07 10:12:42 +1000

    Update LevelTrackerStream.scala

--- level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
+++ level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
@@ -58,7 +58,7 @@ class LevelTrackerStream(levelsChannel: TextChannel)(implicit ex: ExecutionConte
   private lazy val scanForLevels = Flow[Set[CharacterResponse]].mapAsync(1) { characterResponses =>
 		val now = ZonedDateTime.now()
     val newLevels = characterResponses.flatMap { char =>
-			val levels: List[CharKey] = Charkey(char.characters.character.name, Levels(now.toString, char.characters.character.level))
+			val levels: List[CharKey] = List(Charkey(char.characters.character.name, Levels(now.toString, char.characters.character.level)))
       levels.flatMap { l =>
         logger.info(l.toString)
 				logger.info(recentOnline.toString)
