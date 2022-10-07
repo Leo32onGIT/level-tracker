111ce49a8adc8a76761c613d4633c8fb6f4f5905
Leo32 <bwils120@hotmail.com>
2022-10-07 18:19:28 +1000

    Update LevelTrackerStream.scala

--- level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
+++ level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
@@ -61,7 +61,8 @@ class LevelTrackerStream(levelsChannel: TextChannel)(implicit ex: ExecutionConte
 
 			val sheetLevel = char.characters.character.level
 			val name = char.characters.character.name
-			recentOnline.flatMap { ol =>
+			val onlineLevel: List[(String, Int)] = recentOnline.map(i => i.char, i.level.level)
+			onlineLevel.flatMap { ol =>
 				if (ol.char == name){
 					if (ol.level.level > sheetLevel ){
 						val charLevel = CharKey(name, Levels(now.toString, ol.level.level))
