1cd6d4e4cc22f43be4364525ca9b4647720538a5
Leo32 <bwils120@hotmail.com>
2022-10-09 19:12:56 +1000

    Update LevelTrackerStream.scala

--- level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
+++ level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
@@ -71,7 +71,7 @@ class LevelTrackerStream(levelsChannel: TextChannel)(implicit ex: ExecutionConte
               //println(l)
               val lastLoginCheck = l.lastLogin.getOrElse("")
               if (lastLoginCheck != ""){
-                if (l.level > olLevel || ZonedDateTime.parse(l.lastLogin.get).isBefore(ZonedDateTime.parse(sheetLogin.getOrElse("2022-01-01T01:00:00Z")))) {
+                if (l.level > olLevel && ZonedDateTime.parse(l.lastLogin.get).isBefore(ZonedDateTime.parse(sheetLogin.getOrElse("2022-01-01T01:00:00Z")))) {
                   println(s"Level entry deleted:")
                   println(l)
                   recentLevels.remove(l);
