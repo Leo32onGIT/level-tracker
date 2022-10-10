c7dc6681cb2bad6fc1345893311149c7bf453f7c
Leo32 <bwils120@hotmail.com>
2022-10-10 22:24:31 +1000

    Update LevelTrackerStream.scala

--- level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
+++ level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
@@ -48,8 +48,8 @@ class LevelTrackerStream(levelsChannel: TextChannel)(implicit ex: ExecutionConte
 
   private lazy val getCharacterData = Flow[WorldResponse].mapAsync(1) { worldResponse =>
     val online: List[(String, Double)] = worldResponse.worlds.world.online_players.map(i => (i.name, i.level))
-    //recentOnline.filterInPlace(i => !online.map(_._1).contains(i._1)) // Remove existing online chars from the list...
-    recentOnline.filterInPlace(i => false) // idk what im doing, clearing the recentOnline list completely i guess
+    recentOnline.filterInPlace(i => !online.map(_._1).contains(i._1)) // Remove existing online chars from the list...
+    //recentOnline.filterInPlace(i => false) // idk what im doing, clearing the recentOnline list completely i guess
     recentOnline.addAll(online.map(i => (i._1, i._2))) // ...and add them again, with an updated level
 
     // DEBUG:
