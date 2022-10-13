a9b46bd81962c3eba19252826a3f6f5966350de7
Leo32 <bwils120@hotmail.com>
2022-10-13 13:36:56 +1000

    Update LevelTrackerStream.scala

--- level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
+++ level-tracker/src/main/scala/com/kiktibia/leveltracker/LevelTrackerStream.scala
@@ -80,19 +80,17 @@ class LevelTrackerStream(levelsChannel: TextChannel, allyChannel: TextChannel, e
           }
           else None
         }
-        else None
-      }
-
-      // attempt to cleanup recentLevels
-      for (l <- recentLevels){
-        // online char matches recentLevels entry
-        if (olName == l.char){
-          val lastLoginCheck = l.lastLogin.getOrElse("") // safety?
-          if (lastLoginCheck != ""){
-            if (olLevel < l.level || ZonedDateTime.parse(l.lastLogin.getOrElse("2022-01-01T01:00:00Z")).isBefore(ZonedDateTime.parse(sheetLogin.getOrElse("2022-01-01T01:00:00Z")))) {
-              println(s"Online /w Level Entry:\n OL: $olName, $olLevel, ${sheetLogin.getOrElse("Invalid")}\n RL: ${l.char}, ${l.level}, ${l.lastLogin.getOrElse("Invalid")}")
-              println(s"Relogged or died, removing level entry.")
-              recentLevels.remove(l)
+        // attempt to cleanup recentLevels
+        for (l <- recentLevels){
+          // online char matches recentLevels entry
+          if (olName == l.char){
+            val lastLoginCheck = l.lastLogin.getOrElse("") // safety?
+            if (lastLoginCheck != ""){
+              if (olLevel < l.level || ZonedDateTime.parse(l.lastLogin.getOrElse("2022-01-01T01:00:00Z")).isBefore(ZonedDateTime.parse(sheetLogin.getOrElse("2022-01-01T01:00:00Z")))) {
+                println(s"Online /w Level Entry:\n OL: $olName, $olLevel, ${sheetLogin.getOrElse("Invalid")}\n RL: ${l.char}, ${l.level}, ${l.lastLogin.getOrElse("Invalid")}")
+                println(s"Relogged or died, removing level entry.")
+                recentLevels.remove(l)
+              }
             }
           }
         }
